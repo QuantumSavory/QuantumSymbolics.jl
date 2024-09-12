f2cda404f01a28dccbd1748a59b497ba41129149
Stefan Krastanov <stefan@krastanov.org>
2024-09-12 09:36:47 -0400

    simplify JET (#84)

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -2,26 +2,9 @@
 using JET
 using QuantumOptics, QuantumClifford # to load the extensions
 
-using JET: ReportPass, BasicPass, InferenceErrorReport, UncaughtExceptionReport
-
-# Custom report pass that ignores `UncaughtExceptionReport`
-# Too coarse currently, but it serves to ignore the various
-# "may throw" messages for runtime errors we raise on purpose
-# (mostly on malformed user input)
-struct MayThrowIsOk <: ReportPass end
-
-# ignores `UncaughtExceptionReport` analyzed by `JETAnalyzer`
-(::MayThrowIsOk)(::Type{UncaughtExceptionReport}, @nospecialize(_...)) = return
-
-# forward to `BasicPass` for everything else
-function (::MayThrowIsOk)(report_type::Type{<:InferenceErrorReport}, @nospecialize(args...))
-    BasicPass()(report_type, args...)
-end
-
 using InteractiveUtils, Latexify, SymbolicUtils
 
 rep = report_package("QuantumSymbolics";
-    report_pass=MayThrowIsOk(), # TODO have something more fine grained than a generic "do not care about thrown errors"
     ignored_modules=(
         AnyFrameModule(InteractiveUtils),
         AnyFrameModule(Latexify),
@@ -30,5 +13,5 @@ rep = report_package("QuantumSymbolics";
 )
 @show rep
 @test_broken length(JET.get_reports(rep)) == 0
-@test length(JET.get_reports(rep)) <= 7
+@test length(JET.get_reports(rep)) <= 6
 end
