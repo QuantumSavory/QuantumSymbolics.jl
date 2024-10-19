2e9dfb7864e5b10d431f15e384d6d986ee814ae2
Omar Elrefaei <elrefaei.omar@gmail.com>
2024-10-19 11:01:42 -0400

    fix Aqua's reported piracies and method ambiguities

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,7 +1,9 @@
 @testitem "Aqua" tags=[:aqua] begin
     using Aqua
+    import QuantumInterface as QI
+     own_types = [QI.AbstractBra, QI.AbstractKet, QI.AbstractSuperOperator, QI.AbstractOperator]
     Aqua.test_all(QuantumSymbolics,
-            ambiguities=(;broken=true),
-            piracies=(;broken=true),
+            ambiguities=(),
+            piracies=(;treat_as_own=own_types),
     )
 end
