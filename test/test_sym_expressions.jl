de5fba91ac41201a3659d5742d175d8987dd48f1
Stefan Krastanov <stefan@krastanov.org>
2025-02-25 08:57:17 -0500

    improvements to flattening (#103)

--- test/test_sym_expressions.jl
+++ test/test_sym_expressions.jl
@@ -2,8 +2,8 @@
     @test +(Z1) == Z1
     @test +(Z) == Z
     @test isequal(Z1 - Z2, Z1 + (-Z2))
-    @test_broken isequal(Z1 - 2*Z2 + 2*X1, -2*Z2 + Z1 + 2*X1)
-    @test_broken isequal(Z1 - 2*Z2 + 2*X1, Z1 + 2*(-Z2+X1))
+    @test isequal(Z1 - 2*Z2 + 2*X1, -2*Z2 + Z1 + 2*X1)
+    @test isequal(Z1 - 2*Z2 + 2*X1, Z1 + 2*(-Z2+X1))
 
     state1 = XBasisState(1, SpinBasis(1//2))
     state2 = XBasisState(1, SpinBasis(1//2))
@@ -11,4 +11,15 @@
 
     @test isequal(state1, state2)
     @test !isequal(state1, state3)
+
+    @op A
+    @op B
+
+    @test isequal(A+B+A, 2A+B)
+    @test isequal(A+B-A, B)
+    @test A+B-A === B
+    @test 0*A === A-A-2A+2A
+    @test isequal(A+B-A+B, 2B)
+    @test isequal(2A-3B+2(A+B), 4A-B)
+    @test isequal(2A-3B+2(A+2B), 4A+B)
 end
