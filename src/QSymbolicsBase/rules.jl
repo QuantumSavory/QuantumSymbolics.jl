57cf767c10c7d9e47c93a4d451e680cf36c678b9
Sagnik Pal <sagnikpal2004@outlook.com>
2025-04-26 01:59:10 -0400

    updated rules

--- src/QSymbolicsBase/rules.jl
+++ src/QSymbolicsBase/rules.jl
@@ -113,15 +113,21 @@ RULES_ROT = [
     @rule(~r::_isa(RotXGate) => I where (~r).θ == 0),
     @rule(~r::_isa(RotYGate) => I where (~r).θ == 0),
     @rule(~r::_isa(RotZGate) => I where (~r).θ == 0),
+    @rule(~r::_isa(RotXGate) => (-im*X) where (~r).θ in [π, 1π]),
+    @rule(~r::_isa(RotYGate) => (-im*Y) where (~r).θ in [π, 1π]),
+    @rule(~r::_isa(RotZGate) => (-im*Z) where (~r).θ in [π, 1π]),
+    @rule(~r::_isa(RotXGate) => try Rx(mod((~r).θ, 4π)) catch end),
+    @rule(~r::_isa(RotYGate) => try Ry(mod((~r).θ, 4π)) catch end),
+    @rule(~r::_isa(RotZGate) => try Rz(mod((~r).θ, 4π)) catch end),
+    @rule(~r::_isa(RotXGate) => try (~r).θ ≥ 2π ? -Rx((~r).θ - 2π) : nothing catch end),
+    @rule(~r::_isa(RotYGate) => try (~r).θ ≥ 2π ? -Ry((~r).θ - 2π) : nothing catch end),
+    @rule(~r::_isa(RotZGate) => try (~r).θ ≥ 2π ? -Rz((~r).θ - 2π) : nothing catch end),
     @rule(~r1::_isa(RotXGate) * ~r2::_isa(RotXGate) => try Rx((~r1).θ + (~r2).θ) catch end),
     @rule(~r1::_isa(RotYGate) * ~r2::_isa(RotYGate) => try Ry((~r1).θ + (~r2).θ) catch end),
     @rule(~r1::_isa(RotZGate) * ~r2::_isa(RotZGate) => try Rz((~r1).θ + (~r2).θ) catch end),
-    @rule(~r::_isa(RotXGate) => try Rx(mod((~r).θ, 2π)) catch end),
-    @rule(~r::_isa(RotYGate) => try Ry(mod((~r).θ, 2π)) catch end),
-    @rule(~r::_isa(RotZGate) => try Rz(mod((~r).θ, 2π)) catch end),
-    @rule(exp(~α * ~x::_isa(XGate)) => try real(~α) == 0 ? Rx(-2imag(~α)) : nothing catch end),
-    @rule(exp(~α * ~x::_isa(YGate)) => try real(~α) == 0 ? Ry(-2imag(~α)) : nothing catch end),
-    @rule(exp(~α * ~x::_isa(ZGate)) => try real(~α) == 0 ? Rz(-2imag(~α)) : nothing catch end)
+    @rule(exp(~α * ~x::_isa(XGate)) => try if real(~α) == 0 Rx(-2imag(~α)) end catch end),
+    @rule(exp(~α * ~x::_isa(YGate)) => try if real(~α) == 0 Ry(-2imag(~α)) end catch end),
+    @rule(exp(~α * ~x::_isa(ZGate)) => try if real(~α) == 0 Rz(-2imag(~α)) end catch end)
 ]
 
 RULES_SIMPLIFY = [RULES_PAULI; RULES_COMMUTATOR; RULES_ANTICOMMUTATOR; RULES_FOCK; RULES_ROT]
--- test/test_rotation.jl
+++ test/test_rotation.jl
@@ -5,32 +5,54 @@
         @test isequal(qsimplify(Rz(0), rewriter=qsimplify_rot), I)
     end
 
+    @testset "Pauli tests" begin
+        @test isequal(qsimplify(Rx(π), rewriter=qsimplify_rot), -im*X)
+        @test isequal(qsimplify(Ry(π), rewriter=qsimplify_rot), -im*Y)
+        @test isequal(qsimplify(Rz(π), rewriter=qsimplify_rot), -im*Z)
+
+        @test isequal(qsimplify(Rx(1π), rewriter=qsimplify_rot), -im*X)
+        @test isequal(qsimplify(Ry(1π), rewriter=qsimplify_rot), -im*Y)
+        @test isequal(qsimplify(Rz(1π), rewriter=qsimplify_rot), -im*Z)
+    end
+
     @testset "Fusion tests" begin 
-        @test isequal(qsimplify(Rx(π/2) * Rx(π/2), rewriter=qsimplify_rot), Rx(1π))
-        @test isequal(qsimplify(Ry(π/2) * Ry(π/2), rewriter=qsimplify_rot), Ry(1π))
-        @test isequal(qsimplify(Rz(π/2) * Rz(π/2), rewriter=qsimplify_rot), Rz(1π))
+        @test isequal(qsimplify(Rx(π/3) * Rx(π/3), rewriter=qsimplify_rot), Rx(2π/3))
+        @test isequal(qsimplify(Ry(π/3) * Ry(π/3), rewriter=qsimplify_rot), Ry(2π/3))
+        @test isequal(qsimplify(Rz(π/3) * Rz(π/3), rewriter=qsimplify_rot), Rz(2π/3))
 
         @test isequal(qsimplify(Rx(π/2) * Rx(-π/2), rewriter=qsimplify_rot), I)
         @test isequal(qsimplify(Ry(π/2) * Ry(-π/2), rewriter=qsimplify_rot), I)
         @test isequal(qsimplify(Rz(π/2) * Rz(-π/2), rewriter=qsimplify_rot), I)
 
-        @test isequal(qsimplify(2 * Rx(π/2) * Rx(π/2), rewriter=qsimplify_rot), 2Rx(1π))
-        @test isequal(qsimplify(2 * Ry(π/2) * Ry(π/2), rewriter=qsimplify_rot), 2Ry(1π))
-        @test isequal(qsimplify(2 * Rz(π/2) * Rz(π/2), rewriter=qsimplify_rot), 2Rz(1π))
+        @test isequal(qsimplify(2 * Rx(π/2) * Rx(π/2), rewriter=qsimplify_rot), -2im*X)
+        @test isequal(qsimplify(2 * Ry(π/2) * Ry(π/2), rewriter=qsimplify_rot), -2im*Y)
+        @test isequal(qsimplify(2 * Rz(π/2) * Rz(π/2), rewriter=qsimplify_rot), -2im*Z)
     end
 
     @testset "Modulo tests" begin
-        @test isequal(qsimplify(Rx(2π), rewriter=qsimplify_rot), I)
-        @test isequal(qsimplify(Ry(2π), rewriter=qsimplify_rot), I)
-        @test isequal(qsimplify(Rz(2π), rewriter=qsimplify_rot), I)
+        @test isequal(qsimplify(Rx(2π), rewriter=qsimplify_rot), -I)
+        @test isequal(qsimplify(Ry(2π), rewriter=qsimplify_rot), -I)
+        @test isequal(qsimplify(Rz(2π), rewriter=qsimplify_rot), -I)
 
-        @test isequal(qsimplify(Rx(π) * Rx(π), rewriter=qsimplify_rot), I)
-        @test isequal(qsimplify(Ry(π) * Ry(π), rewriter=qsimplify_rot), I)
-        @test isequal(qsimplify(Rz(π) * Rz(π), rewriter=qsimplify_rot), I)
+        @test isequal(qsimplify(Rx(3π), rewriter=qsimplify_rot), im*X)
+        @test isequal(qsimplify(Ry(3π), rewriter=qsimplify_rot), im*Y)
+        @test isequal(qsimplify(Rz(3π), rewriter=qsimplify_rot), im*Z)
 
-        @test isequal(qsimplify(2 * Rx(3π/2) * Rx(2π/2), rewriter=qsimplify_rot), 2Rx(π/2))
-        @test isequal(qsimplify(2 * Ry(3π/2) * Ry(2π/2), rewriter=qsimplify_rot), 2Ry(π/2))
-        @test isequal(qsimplify(2 * Rz(3π/2) * Rz(2π/2), rewriter=qsimplify_rot), 2Rz(π/2))
+        @test isequal(qsimplify(Rx(4π), rewriter=qsimplify_rot), I)
+        @test isequal(qsimplify(Ry(4π), rewriter=qsimplify_rot), I)
+        @test isequal(qsimplify(Rz(4π), rewriter=qsimplify_rot), I)
+
+        @test isequal(qsimplify(Rx(5π), rewriter=qsimplify_rot), -im*X)
+        @test isequal(qsimplify(Ry(5π), rewriter=qsimplify_rot), -im*Y)
+        @test isequal(qsimplify(Rz(5π), rewriter=qsimplify_rot), -im*Z)
+
+        @test isequal(qsimplify(Rx(π) * Rx(π), rewriter=qsimplify_rot), -I)
+        @test isequal(qsimplify(Ry(π) * Ry(π), rewriter=qsimplify_rot), -I)
+        @test isequal(qsimplify(Rz(π) * Rz(π), rewriter=qsimplify_rot), -I)
+
+        @test isequal(qsimplify(2 * Rx(3π/2) * Rx(2π/2), rewriter=qsimplify_rot), -2Rx(π/2))
+        @test isequal(qsimplify(2 * Ry(3π/2) * Ry(2π/2), rewriter=qsimplify_rot), -2Ry(π/2))
+        @test isequal(qsimplify(2 * Rz(3π/2) * Rz(2π/2), rewriter=qsimplify_rot), -2Rz(π/2))
     end
 
     @testset "Exponential tests" begin
