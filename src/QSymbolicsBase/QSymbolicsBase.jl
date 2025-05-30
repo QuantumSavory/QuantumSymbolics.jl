d903bfa5215a7bda2991c6121a335151699693f7
Jeffrey Wack <jeffwack111@gmail.com>
2025-05-30 01:41:08 -0700

    start of QuantumToolboxExt

--- src/QSymbolicsBase/QSymbolicsBase.jl
+++ src/QSymbolicsBase/QSymbolicsBase.jl
@@ -22,7 +22,7 @@ import QuantumInterface:
 
 export SymQObj,QObj,
        AbstractRepresentation,AbstractUse,
-       QuantumOpticsRepr,QuantumMCRepr,CliffordRepr,
+       QuantumOpticsRepr,QuantumMCRepr,CliffordRepr,QuantumToolboxRepr,
        UseAsState,UseAsObservable,UseAsOperation,
        apply!,
        express,
@@ -50,6 +50,15 @@ export SymQObj,QObj,
        qexpand,
        isunitary,
        KrausRepr,kraus
+
+
+
+
+#Move this to QuantumInterface?
+"""Representation using kets, bras, density matrices, and superoperators governed by `QuantumToolbox.jl`."""
+Base.@kwdef struct QuantumToolboxRepr <: AbstractRepresentation 
+    cutoff::Int = 2
+end
 
 ##
 # Metadata cache helpers
