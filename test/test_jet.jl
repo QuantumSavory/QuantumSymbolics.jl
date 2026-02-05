e8bc9025f075590c3f378e199df26b1d477cc931
Stefan Krastanov <github.acc@krastanov.org>
2026-02-05 10:01:38 -0500

    fixup

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -1,9 +1,10 @@
 @testitem "JET checks" tags=[:jet] begin
     using JET
+    using QuantumOptics, QuantumClifford # to load the extensions
     using Test
-    using PBCCompiler
+    using QuantumSymbolics
 
-    rep = JET.report_package(PBCCompiler, target_modules=[PBCCompiler])
+    rep = JET.report_package(QuantumSymbolics, target_modules=[QuantumSymbolics])
     @show rep
     @test_broken length(JET.get_reports(rep)) == 0
     @test length(JET.get_reports(rep)) <= 6
