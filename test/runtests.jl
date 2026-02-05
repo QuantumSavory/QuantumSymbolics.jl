34b8f44ff546dde0db2769df836af973bad38967
Stefan Krastanov <github.acc@krastanov.org>
2026-02-05 09:12:04 -0500

    cleanup for buildkite/jet/downstream tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,32 @@
-using TestItemRunner
 using QuantumSymbolics
+using TestItemRunner
+
+JET_flag = false
+
+if get(ENV, "JET_TEST", "") != "true"
+    @info "Skipping JET tests -- must be explicitly enabled."
+    @info "Environment must set JET_TEST=true."
+else
+    JET_flag = true
+end
+
+using Pkg
+JET_flag && Pkg.add("JET")
 
 # filter for the test
 testfilter = ti -> begin
-  exclude = Symbol[:jet]
-  if !(VERSION >= v"1.10")
-    push!(exclude, :doctests)
-    push!(exclude, :aqua)
-  end
+    exclude = Symbol[]
+    if !JET_flag
+        push!(exclude, :jet)
+    end
+    if !(VERSION >= v"1.10")
+        push!(exclude, :doctests)
+        push!(exclude, :aqua)
+    end
 
-  return all(!in(exclude), ti.tags)
+    return all(!in(exclude), ti.tags)
 end
 
 println("Starting tests with $(Threads.nthreads()) threads out of `Sys.CPU_THREADS = $(Sys.CPU_THREADS)`...")
 
 @run_package_tests filter=testfilter
-
-if get(ENV,"JET_TEST","")=="true"
-    @run_package_tests filter=(ti -> :jet in ti.tags)
-end
--- test/test_jet.jl
+++ test/test_jet.jl
@@ -1,17 +1,10 @@
 @testitem "JET checks" tags=[:jet] begin
-using JET
-using QuantumOptics, QuantumClifford # to load the extensions
+    using JET
+    using Test
+    using PBCCompiler
 
-using InteractiveUtils, Latexify, SymbolicUtils
-
-rep = report_package("QuantumSymbolics";
-    ignored_modules=(
-        AnyFrameModule(InteractiveUtils),
-        AnyFrameModule(Latexify),
-        AnyFrameModule(SymbolicUtils)
-    )
-)
-@show rep
-@test_broken length(JET.get_reports(rep)) == 0
-@test length(JET.get_reports(rep)) <= 6
+    rep = JET.report_package(PBCCompiler, target_modules=[PBCCompiler])
+    @show rep
+    @test_broken length(JET.get_reports(rep)) == 0
+    @test length(JET.get_reports(rep)) <= 6
 end
